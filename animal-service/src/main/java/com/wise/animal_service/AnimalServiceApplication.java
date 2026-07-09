403954e73ef07faebb1b9e79bbe490e475e64af3
Viktoryia Buiko <viktoryia.buiko@innowise.com>
2026-07-09 11:21:59 +0200

    initial microservices project structure

---
+++ animal-service/src/main/java/com/wise/animal_service/AnimalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wise.animal_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalServiceApplication.class, args);
+	}
+
+}
---
+++ animal-service/src/test/java/com/wise/animal_service/AnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wise.animal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
